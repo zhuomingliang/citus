b035cddffdf8478aae7c158819dfad420798c576
Onder Kalaci <onder@citusdata.com>
2019-12-24 13:00:13 +0100

    Fix

--- ci/editorconfig.sh
+++ ci/editorconfig.sh
@@ -7,7 +7,8 @@ for f in $(git ls-tree -r HEAD --name-only); do
         [ "$(dirname "$f")" != "src/test/regress/output" ]
     then
         # Trim trailing whitespace
-        sed -e 's/[[:space:]]*$//' -i "./$f"
+        sed -i".backup" -e 's/[[:space:]]*$//' "./$f"
+        rm "./$f.backup"
         # Add final newline if not there
         if [ -n "$(tail -c1 "$f")" ]; then
             echo >> "$f"
