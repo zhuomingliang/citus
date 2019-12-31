50f6b8b12f393b8f34ed9f190163975b50dbc389
Hanefi Onaldi <hanefi@citusdata.com>
2019-12-31 14:15:00 +0300

    failed attempt 1: write to local if there are no task placements

--- src/backend/distributed/planner/intermediate_result_pruning.c
+++ src/backend/distributed/planner/intermediate_result_pruning.c
@@ -180,7 +180,7 @@ AppendAllAccessedWorkerNodes(IntermediateResultsHashEntry *entry,
 		/* if there is a task that does not have any placements, write to local file
 		 * TODO: Hanefi make sure that it is possible to have nil task placement lists
 		 */
-		if (list_length(task->taskPlacementList) < 3)
+		if (list_length(task->taskPlacementList) < 1)
 		{
 			entry->writeLocalFile = true;
 		}
