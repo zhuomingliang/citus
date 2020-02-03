c14cb1ff30249eacd0ae7ffa137bc3334b5a38a2
Hadi Moshayedi <hadi@moshayedi.net>
2020-02-03 13:13:09 -0800

    Use current user for repartition join temp schemas.
    Otherwise when using a less privileged user we might get
    errors when trying to create the schema.

--- src/backend/distributed/sql/udfs/worker_create_schema/latest.sql
+++ src/backend/distributed/sql/udfs/worker_create_schema/latest.sql
@@ -1,8 +1,10 @@
-CREATE FUNCTION pg_catalog.worker_create_schema(bigint)
+DROP FUNCTION IF EXISTS pg_catalog.worker_create_schema(jobid bigint);
+
+CREATE FUNCTION pg_catalog.worker_create_schema(jobid bigint, userid oid)
     RETURNS void
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$worker_create_schema$$;
-COMMENT ON FUNCTION pg_catalog.worker_create_schema(bigint)
+COMMENT ON FUNCTION pg_catalog.worker_create_schema(bigint, oid)
     IS 'create schema in remote node';
 
-REVOKE ALL ON FUNCTION pg_catalog.worker_create_schema(bigint) FROM PUBLIC;
+REVOKE ALL ON FUNCTION pg_catalog.worker_create_schema(bigint, oid) FROM PUBLIC;
