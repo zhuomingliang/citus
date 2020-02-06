9fc9a2651d6ef6c7fdfc423c65e3fa27b21136bd
Marco Slot <marco.slot@gmail.com>
2020-02-06 13:42:12 +0100

    Azure orcas breadth

--- src/include/distributed/log_utils.h
+++ src/include/distributed/log_utils.h
@@ -20,11 +20,25 @@ extern char * HashLogMessage(const char *text);
 	(log_min_messages <= ereport_loglevel ? HashLogMessage(text) : text)
 
 #undef ereport
+
+/*
+ * The AZURE_ORCAS_BREADTH variant of PostgreSQL has two extra parameters for private
+ * and internal event logging in ereport_domain..
+ */
+#ifdef AZURE_ORCAS_BREADTH
+#define ereport(elevel, rest) \
+	do { \
+		int ereport_loglevel = elevel; \
+		(void) (ereport_loglevel); \
+		ereport_domain(elevel, TEXTDOMAIN, rest, false, false); \
+	} while (0)
+#else
 #define ereport(elevel, rest) \
 	do { \
 		int ereport_loglevel = elevel; \
 		(void) (ereport_loglevel); \
 		ereport_domain(elevel, TEXTDOMAIN, rest); \
 	} while (0)
+#endif
 
 #endif /* LOG_UTILS_H */
